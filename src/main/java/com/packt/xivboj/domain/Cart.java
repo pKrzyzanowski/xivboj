7ec5dcdc7da9e11bcb969f30b6cb8c5093c44def
Patryk Krzyz <d@fi.com>
2018-08-10 14:26:49 +0200

    create Cart(koszyk) i CartItemComp(konkurencja w koszyku) klas

---
+++ src/main/java/com/packt/xivboj/domain/Cart.java
@@ -0,0 +1,53 @@
+package com.packt.xivboj.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+
+    private String cartId;
+    private Map<String, CartItemCompe> cartItems;
+
+    public Cart() {
+        cartItems = new HashMap<String, CartItemCompe>();
+    }
+
+    public Cart(String cartId) {
+        this();
+        this.cartId = cartId;
+    }
+
+    public String getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(String cartId) {
+        this.cartId = cartId;
+    }
+
+    public Map<String, CartItemCompe> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(Map<String, CartItemCompe> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public void addCartItem(CartItemCompe cartItemCompe) {
+        String competitionId = cartItemCompe.getCompetition().getCompetitionId();
+
+        if (cartItems.containsKey(competitionId)) {
+
+            // tutaj bedzie raczej, ze jezeli istnieje juz taki produkt to pojawia sie okienko ze taki juz istnieje
+
+        }
+        else{
+            cartItems.put(competitionId, cartItemCompe);
+        }
+    }
+
+    public void removeCartItem(CartItemCompe cartItemCompe) {
+        String competitionId = cartItemCompe.getCompetition().getCompetitionId();
+        cartItems.remove(competitionId);
+    }
+}
---
+++ src/main/java/com/packt/xivboj/domain/CartItemCompe.java
@@ -0,0 +1,17 @@
+package com.packt.xivboj.domain;
+
+public class CartItemCompe {
+    private Competition competition;
+
+
+    public CartItemCompe() {
+    }
+
+    public Competition getCompetition() {
+        return competition;
+    }
+
+    public void setCompetition(Competition competition) {
+        this.competition = competition;
+    }
+}
