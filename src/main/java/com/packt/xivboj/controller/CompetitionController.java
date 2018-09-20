376c00e8ac232001850c9b596aa56e62593e4c8a
Patryk Krzyz <d@fi.com>
2018-09-20 10:52:23 +0200

    enable registration to dataBase

--- src/main/java/com/packt/xivboj/controller/CompetitionController.java
+++ src/main/java/com/packt/xivboj/controller/CompetitionController.java
@@ -5,6 +5,8 @@ import com.packt.xivboj.domain.Competition;
 import com.packt.xivboj.service.CompetitionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -29,8 +31,8 @@ public class CompetitionController {
     @RequestMapping
     public String list(Model model) {
         model.addAttribute("competitions", competitionService.getAllCompetitions());
-//                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        String currentPrincipalName = authentication.getName();
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
 //        String a = "asd";
         competitionService.initializeBasicCompetitions();
         return "competitions";
